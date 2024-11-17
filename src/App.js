26c76b1a6edbece7ce89035b9727953217070ee5
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-11-17 18:39:00 +0530

    login page and signin page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar";
 import {Routes,Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Account from "./pages/Account";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/account" element={<Account/>}/>
       </Routes>
 
     </AuthContextProvider>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1 className='text-white'>hello</h1>
+      
+    </div>
+  )
+}
+
+export default Login
